437d2685f9c98f1ce64b18694401d9e2c43ef0c4
Roberto Samia <bojosteph@gmail.com>
2018-09-23 17:30:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  
+  arr.each_with_index{|value , index| puts "#{index += 1} item is #{value}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (a)# code an argument here
   # Your code here
+  a.map! {|x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls ()# code an argument here
+  all? do |e|
+    e.length >= 4
 end
 
 def find_the_cheese# code an argument here
